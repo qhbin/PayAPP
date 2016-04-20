7c25e9626bc151d549f259b213015d9e92f61ee6
binbin <925078922@qq.com>
2016-04-20 12:59:49 +0800

    增加重设密码输入手机页面

---
+++ app/src/main/java/com/binbin/activity/RsetPassword.java
@@ -0,0 +1,18 @@
+package com.binbin.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.binbin.activity.R;
+
+public class RsetPassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rset_password);
+    }
+    public void next(){}
+}
